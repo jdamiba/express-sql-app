a581b3e3a702a7c54049732b09dbf0c847787d55
Joseph Damiba <joseph.damiba@plot.ly>
2020-01-30 20:51:11 -0500

    setup

---
+++ models/users.js
@@ -0,0 +1,43 @@
+var Sequelize = require('sequelize');
+var bcrypt = require('bcrypt');
+
+// create a sequelize instance with our local postgres database information.
+var sequelize = new Sequelize('mysql://bbadfc6ae46150:e441bb86@us-cdbr-iron-east-04.cleardb.net/heroku_c2aee30971014a9?reconnect=true');
+
+// setup User model and its fields.
+var User = sequelize.define('users', {
+    username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}, {
+    hooks: {
+      beforeCreate: (user) => {
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(user.password, salt);
+      }
+    },
+    instanceMethods: {
+      validPassword: function(password) {
+        return bcrypt.compareSync(password, this.password);
+      }
+    }    
+});
+
+// create all the defined tables in the specified database.
+sequelize.sync()
+    .then(() => console.log('users table has been successfully created, if one doesn\'t exist'))
+    .catch(error => console.log('This error occured', error));
+
+// export User model for use in other files.
+module.exports = User;
